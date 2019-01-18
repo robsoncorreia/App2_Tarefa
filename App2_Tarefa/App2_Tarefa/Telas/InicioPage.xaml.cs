7d07e97a3d35d94b11e553a7bf15ae5c4bfe76e5
Robson Correia <robsoncorreia01@outlook.com>
2019-01-17 23:13:52 -0200

    Adicionar arquivos de projeto.

---
+++ App2_Tarefa/App2_Tarefa/Telas/InicioPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2_Tarefa.Telas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class InicioPage : ContentPage
+	{
+		public InicioPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void ActionGoCadastro(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CadastroPage());
+        }
+    }
+}
